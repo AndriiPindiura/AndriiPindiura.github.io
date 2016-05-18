84f17ff9f492ec9091d017aa7cbb7018e047ba66
Andrii Pindiura <aydnep@aydnep.com.ua>
2016-05-18 17:22:57 +0300

    ned

--- GoIT/javascript/home/lesson1-2/js/task2.js
+++ GoIT/javascript/home/lesson1-2/js/task2.js
@@ -1,23 +1,26 @@
 var names = [5];
-for (var i = 0; i < 5; i++)
-{
-    names[i] = prompt('Input name ' + (i + 1) + ': ');
+for (var i = 0; i < 5; i++) {
+    var name = prompt('Input name ' + (i + 1) + ': ');
+    if (name === '') {
+        console.log('buy buy!');
+        break;
+    }
+    else {
+        names[i] = name;
+    }
 }
 
 var userName = prompt("Input username: ");
-
 if (!compareNames(userName)) {
     alert("Error!\nUser not found");
 }
 
 function compareNames(name) {
-    for (var i = 0, l = names.length; i < l; i++)
-    {
-       if (names[i] == name)
-       {
-           alert(name + ', вы успешно вошли');
-           return true;
-       }
+    for (var i = 0, l = names.length; i < l; i++) {
+        if (names[i] == name) {
+            alert(name + ', вы успешно вошли');
+            return true;
+        }
     }
     return false;
 }
