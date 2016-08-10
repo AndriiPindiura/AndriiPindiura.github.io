8b36029a544f7945e2dc4a17d9643236a565ef18
Andrii Pindiura <aydnep@aydnep.com.ua>
2016-08-10 08:45:36 +0300

    add es6-promise polyfill

--- GoIT/markup/exam/react/src/index.js
+++ GoIT/markup/exam/react/src/index.js
@@ -2,6 +2,10 @@ import 'core-js/fn/object/assign';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/Main';
+import ES6Promise from 'es6-promise';
 require('./components/main.scss');
+
+ES6Promise.polyfill();
+
 // Render the main component into the dom
 ReactDOM.render(<App />, document.getElementById('app'));
