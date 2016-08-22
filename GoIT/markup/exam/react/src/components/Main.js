c8a94c883b30dad75b797efb2c3c72cf03e873c3
Andrii Pindiura <aydnep@aydnep.com.ua>
2016-08-22 18:50:41 +0300

    apply mentor comments

--- GoIT/markup/exam/react/src/components/Main.js
+++ GoIT/markup/exam/react/src/components/Main.js
@@ -6,11 +6,21 @@ import HowItWork from './howitwork';
 import Partners from './partners';
 import Ideas from './ideas';
 import Footer from './footer';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Dialog from 'material-ui/Dialog';
+import styles from './dialog/main.scss';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
+
 
 class AppComponent extends React.Component {
 	constructor() {
 		super();
-		this.state = { breakPoint: breakpoints.currentBreakpoint() };
+		this.state = { breakPoint: breakpoints.currentBreakpoint(), idea: null };
+		this.showIdea = this.showIdea.bind(this);
 	}
 
 	componentDidMount() {
@@ -18,16 +28,76 @@ class AppComponent extends React.Component {
 			this.setState({ breakPoint: breakpoints.currentBreakpoint() });
 		});
 	}
-
+	showIdea(idea) {
+		console.log(idea);
+		this.setState({ idea });
+	}
 	render() {
-		return (
-			<div className="urlaubsgluck">
-				<Promo breakpoint={this.state.breakPoint}/>
-				<HowItWork breakpoint={this.state.breakPoint}/>
-				<Partners />
-				<Ideas />
-				<Footer />
+		const idea = this.state.idea ?
+			<Dialog
+				//className="qwerty"
+				//title="Scrollable Dialog"
+				//titleStyle={{ background: 'rgba(0, 0, 0, 0)' }}
+				//actionsContainerStyle={{ background: 'rgba(0, 0, 0, 0)' }}
+				//actionsContainerStyle={{ display: 'none' }}
+				className={styles.dialog}
+				// contentClassName={styles.dialog}
+				contentStyle={{ width: 'auto', maxWidth: 'initial', display: 'inline-block' }}
+				//contentStyle={{ background: `url(${this.state.idea}) no-repeat center`, height: '75vh' }}
+				// bodyClassName="body"
+				// bodyStyle={{ background: 'rgba(0, 0, 0, 0)' }}
+				// actionsContainerClassName="actions"
+				// overlayClassName="overlay"
+				// titleClassName="title"
+				//actions = {[(<button onClick={() => this.setState({ idea: null })}>close</button>)]}
+				modal={false}
+				open={this.state.idea ? true : false}
+				// open={this.state.open}
+				// onRequestClose={this.handleClose}
+				autoScrollBodyContent={true}
+			>
+			<nav>
+				<a href={this.state.idea.source} target="_blank">Read more...</a>
+				<button onClick={() => this.setState({ idea: null })}>
+					<svg width="12px" height="12px" viewBox="0 0 12 12">
+						<g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+							<g id="MyMovies" transform="translate(-960.000000, -131.000000)">
+								<g id="Moview" transform="translate(35.000000, 118.000000)">
+									<g id="ic_remove" transform="translate(922.000000, 10.000000)">
+										<path
+											d="M0,0 L18,0 L18,18 L0,18 L0,0 Z M0,0 L18,0 L18,18 L0,18 L0,0 Z"
+											id="Shape"
+										/>
+										<polygon
+											id="Shape"
+											fill="#9B9B9B"
+											points="14.53 4.53 13.47 3.47 9 7.94 4.53 3.47 3.47 4.53 7.94 9 3.47 13.47 4.53 14.53 9 10.06 13.47 14.53 14.53 13.47 10.06 9"
+										/>
+									</g>
+								</g>
+							</g>
+						</g>
+					</svg>
+				</button>
+			</nav>
+			<div>
+				<img src={this.state.idea.image} />
 			</div>
+
+			</Dialog>
+			: false;
+
+		return (
+			<MuiThemeProvider>
+				<div>
+					<Promo breakpoint={this.state.breakPoint}/>
+					<HowItWork breakpoint={this.state.breakPoint}/>
+					<Partners />
+					{idea}
+					<Ideas callback={this.showIdea}/>
+					<Footer />
+				</div>
+			</MuiThemeProvider>
 		);
 	}
 }
